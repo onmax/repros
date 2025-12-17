bf842a6919ad9d124b79c87e2e71288826a88406
onmax <maximogarciamtnez@gmail.com>
2025-12-17 09:05:26 +0100

    add nuxthub-727 mysql mode repro

---
+++ nuxthub-727-fixed/nuxt.config.ts
@@ -0,0 +1,10 @@
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  modules: ['@nuxthub/core'],
+  hub: {
+    db: {
+      dialect: 'mysql',
+      mode: 'default',
+    },
+  },
+})
