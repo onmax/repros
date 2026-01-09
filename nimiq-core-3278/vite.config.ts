c0006acf4b694fbf208f45d7484ca648f9056dbf
onmax <maximogarciamtnez@gmail.com>
2026-01-09 15:00:11 +0100

    add comlink plugin, copy public files

--- nimiq-core-3278/vite.config.ts
+++ nimiq-core-3278/vite.config.ts
@@ -1,13 +1,39 @@
+import { copyFile, cp, mkdir } from 'node:fs/promises'
+import path from 'node:path'
+import { fileURLToPath, URL } from 'node:url'
+
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import topLevelAwait from 'vite-plugin-top-level-await'
 import wasm from 'vite-plugin-wasm'
+
+function copyComlinkPlugin() {
+  return {
+    name: 'copy-comlink-worker-dependency',
+    apply: 'build',
+    async closeBundle() {
+      const publicDir = fileURLToPath(new URL('./public', import.meta.url))
+      const distDir = fileURLToPath(new URL('./dist', import.meta.url))
+      const distAssetsDir = path.join(distDir, 'assets')
+      const src = path.join(publicDir, 'comlink.min.js')
+      const dest = path.join(distAssetsDir, 'comlink.min.js')
+
+      await mkdir(distAssetsDir, { recursive: true })
+      await copyFile(src, dest)
+
+      const workerSrcDir = path.join(distDir, 'worker-wasm')
+      const workerDestDir = path.join(distAssetsDir, 'worker-wasm')
+      await cp(workerSrcDir, workerDestDir, { recursive: true })
+    },
+  }
+}
 
 export default defineConfig({
   plugins: [
     vue(),
     wasm(),
     topLevelAwait(),
+    copyComlinkPlugin(),
   ],
   worker: {
     format: 'es',
