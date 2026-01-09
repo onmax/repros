a3fa089ca7c0e5b74a48b4b083b050159cf9d2ec
onmax <maximogarciamtnez@gmail.com>
2026-01-09 15:09:57 +0100

    add cloudflare workers setup (wip)

---
+++ nimiq-core-3278/dist/composables/useNimiq.js
@@ -0,0 +1,48 @@
+import init, * as Nimiq from '@nimiq/core/web';
+import { onUnmounted, ref } from 'vue';
+export function useNimiq() {
+    const client = ref(null);
+    const loading = ref(false);
+    const error = ref();
+    const consensus = ref('Not connected');
+    const headBlockNumber = ref(0);
+    async function initializeNimiq() {
+        if (client.value)
+            return;
+        try {
+            loading.value = true;
+            error.value = undefined;
+            await init();
+            const config = new Nimiq.ClientConfiguration();
+            config.syncMode('pico');
+            client.value = await Nimiq.Client.create(config.build());
+            client.value.addConsensusChangedListener((state) => {
+                consensus.value = state;
+            });
+            client.value.addHeadChangedListener(async (hash) => {
+                const block = await client.value.getBlock(hash);
+                if (block) {
+                    headBlockNumber.value = block.height;
+                }
+            });
+        }
+        catch (err) {
+            error.value = err instanceof Error ? err.message : JSON.stringify(err);
+            client.value = null;
+        }
+        finally {
+            loading.value = false;
+        }
+    }
+    onUnmounted(() => {
+        client.value = null;
+    });
+    return {
+        initializeNimiq,
+        client,
+        loading,
+        error,
+        consensus,
+        headBlockNumber,
+    };
+}
