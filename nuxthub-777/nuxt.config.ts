21d03a64e836cb90efcbcf9247a91827385391f7
onmax <maximogarciamtnez@gmail.com>
2026-01-20 16:17:13 +0100

    nuxthub-777: lazy db url resolution for non-CF

--- nuxthub-777/nuxt.config.ts
+++ nuxthub-777/nuxt.config.ts
@@ -1,5 +1,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-01-01',
   modules: ['@nuxthub/core'],
-  hub: { db: 'postgresql' }
+  hub: {
+    db: {
+      dialect: 'postgresql',
+      driver: 'postgres-js',
+      applyMigrationsDuringBuild: false
+    }
+  }
 })
