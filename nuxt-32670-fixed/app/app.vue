c1e9dd499aca0838408b5ef2a953b22fc710a7fb
onmax <maximogarciamtnez@gmail.com>
2026-01-23 21:26:49 +0100

    add nuxt-32670 repro

---
+++ nuxt-32670-fixed/app/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <Html>
+      <Link rel="alternate" href="/" hreflang="en" />
+      <Link rel="canonical" href="/" />
+    </Html>
+    <h1>Link Duplication Test</h1>
+    <p>Check DevTools: link[rel="alternate"] should appear once, not twice</p>
+  </div>
+</template>
