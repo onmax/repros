8eee227bb0e8880c10789bdec6aefbe66f12d9eb
onmax <maximogarciamtnez@gmail.com>
2026-09-05 06:59:53 +0000

    chore: isolate repro/nuxt-33824 reproduction

---
+++ nuxt-33824-fixed/app/pages/main/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Main Page</h1>
+    <nav>
+      <NuxtLink to="/page-a">Page A</NuxtLink>
+      <NuxtLink to="/page-b">Page B</NuxtLink>
+    </nav>
+  </div>
+</template>
---
+++ nuxt-33824-fixed/app/pages/page-a/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Page A</h1>
+    <NuxtLink to="/main">Back to Main</NuxtLink>
+  </div>
+</template>
