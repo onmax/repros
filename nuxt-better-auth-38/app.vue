3f1030f5454cc988695b0e69bab224a336316df6
onmax <maximogarciamtnez@gmail.com>
2026-09-05 06:59:54 +0000

    chore: isolate repro/nuxt-better-auth-38 reproduction

---
+++ nuxt-better-auth-38/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Client-only Auth Test</h1>
+    <p v-if="loggedIn">Logged in as {{ user?.email }}</p>
+    <p v-else>Not logged in</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { loggedIn, user } = useUserSession()
+</script>
