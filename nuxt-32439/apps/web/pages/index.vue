6de6de6702500dcacf5edb5727b53632074c0665
onmax <maximogarciamtnez@gmail.com>
2026-09-05 06:59:53 +0000

    chore: isolate repro/nuxt-32439 reproduction

---
+++ nuxt-32439/apps/web/pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <main>
+    <p>query preview: {{ route.query.preview }}</p>
+    <div
+      :style="{ backgroundColor: route.query.preview === 'true' ? 'red' : 'blue' }"
+    >
+      expected red when ?preview=true
+    </div>
+  </main>
+</template>
