af2dd12947c71eb668fdf5e1e429b8d68b224c1f
onmax <maximogarciamtnez@gmail.com>
2026-09-05 06:59:57 +0000

    chore: isolate repro/nuxthub-809 reproduction

---
+++ nuxthub-809-fix/app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const upload = useMultipartUpload('/api/blob')
+</script>
+
+<template>
+  <div>
+    <h1>NuxtHub #809 Repro</h1>
+    <p>Check console for Vite import error</p>
+  </div>
+</template>
